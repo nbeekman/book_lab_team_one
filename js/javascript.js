e88b7135af11cf02a30381bb7b30654fcf84b773
nbeekman <nathan.beekman1@gmail.com>
2013-06-07 14:45:35 -0600

    booking slider

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  var width = $(window).width();
+  
+  if (width > 401) {
+      $("#booking li:last-child").mouseover(function(){
+        $("#booking").css("left", "0px");
+      });
+      $("#booking").mouseleave(function(){
+        $("#booking").css("left", "-460px");
+      });
+  } else {
+      $("#booking li:first-child").mouseover(function(){
+        $("#booking").css("bottom", "0px");
+      });
+      $("#booking").mouseleave(function(){
+        $("#booking").css("bottom", "-510px");
+      });
+  }
+});
+
