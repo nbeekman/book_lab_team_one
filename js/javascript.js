9bcc91a97607a11f7da287a5fc2a0835be1205a9
nbeekman <nathan.beekman1@gmail.com>
2013-06-07 14:51:23 -0600

    minor changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,18 @@
-$(document).ready(function(){
-  var width = $(window).width();
+var width = $(window).width();
   
-  if (width > 401) {
-      $("#booking li:last-child").mouseover(function(){
-        $("#booking").css("left", "0px");
-      });
-      $("#booking").mouseleave(function(){
-        $("#booking").css("left", "-460px");
-      });
-  } else {
-      $("#booking li:first-child").mouseover(function(){
-        $("#booking").css("bottom", "0px");
-      });
-      $("#booking").mouseleave(function(){
-        $("#booking").css("bottom", "-510px");
-      });
-  }
-});
+if (width > 401) {
+    $("#booking li:last-child").mouseover(function(){
+      $("#booking").css("left", "0px");
+    });
+    $("#booking").mouseleave(function(){
+      $("#booking").css("left", "-460px");
+    });
+} else {
+    $("#booking li:first-child").mouseover(function(){
+      $("#booking").css("bottom", "0px");
+    });
+    $("#booking").mouseleave(function(){
+      $("#booking").css("bottom", "-510px");
+    });
+}
 
