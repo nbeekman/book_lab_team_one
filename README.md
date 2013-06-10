01837b32a96465e60ac38684a0e37f1b81290967
Your Name <you@domain.com>
2013-06-09 22:31:34 -0600

    Up and running on rails.

--- README.md
+++
@@ -1,4 +0,0 @@
-Team One - Book Lab
----
-
-Website redesign of Ritz Carlton (www.ritzcarlton.com)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BdwPrototypeRitzcarlton::Application.config.secret_token = 'ad79bb42de5b08eec614d1cae6ca9c30270bb64a00d6cb85c35d2eda55b13fc9a4e3ecd929796a570354d82d50e02bf2af946bbbbf3404d2ede143342123dded'
